73ad34ef5bcbca433310bdf63d28a4af00c9cfe8
Guilherme <guilhermesantanapessa12@gmail.com>
2024-05-08 13:34:20 -0300

    Adicionando ReadMe.cs

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# WebStore
+
+- This repository aims to create a simple WebStore simulating the real scenario.
+
+Things i will implement: 
+
+1. Connection with a database: MySql or Postgres  
+2. Define a secutrity layer with JWT
+3. Use a payment method with an external API
+4. Use tests
--- app/src/obj/Debug/net8.0/Produtos.AssemblyInfo.cs
+++ app/src/obj/Debug/net8.0/Produtos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Produtos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa2c20b7a0d0cbf0a842f1a22b572c7b6d2419e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Produtos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Produtos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
